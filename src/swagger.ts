69061aee6d6b2b1dbab516b0fd3fc2d02dfefce4
Harshiv Patel <harshiv@stratosfy.io>
2024-03-10 05:09:55 -0400

    fix

--- src/swagger.ts
+++ src/swagger.ts
@@ -117,7 +117,7 @@ export const setupSwagger = (app: any) => {
 
   const swaggerDocs = swaggerJsDoc(swaggerOptions);
   const CSS_URL =
-    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css";
 
   app.use(
     "/api-docs",
